e67408b6a5e2b1c107670faab589513bba6ec728
Filip Herman <filipherman@hotmail.com>
2026-02-19 18:11:31 +0100

    Initial Commit

---
+++ FridgeManager/FridgeManagerApp.swift
@@ -0,0 +1,32 @@
+//
+//  FridgeManagerApp.swift
+//  FridgeManager
+//
+//  Created by Filip Herman on 19/02/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FridgeManagerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
